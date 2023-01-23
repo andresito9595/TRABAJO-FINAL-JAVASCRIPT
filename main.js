69cca97f0e00a73688b04edcc3a86a4737b775e9
andres mercado <andres95rino@gmail.com>
2023-01-23 14:14:32 -0300

    CARD

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    autoplay: {
+        delay: 3500
+    }
+});
+
 const SEARCH_ICON = document.querySelector('.search-icon')
 const IMPUT_SEARCH = document.querySelector('.input-search')
 
@@ -10,15 +25,38 @@ const deployMenu = (e) => {
 
 }
 
+const searching = (e) => {
 
+    console.log(e.target.value)
+
+}
 
 
 
 const init = () => {
 
     SEARCH_ICON.addEventListener('click', deployMenu)
+    IMPUT_SEARCH.addEventListener('keyup', searching)
 }
 
 
 
 init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

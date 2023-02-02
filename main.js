f3f37687cbaf187337200497265c978d7714c18e
andres mercado <andres95rino@gmail.com>
2023-02-01 22:34:25 -0300

    menu-and-submenu

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.brow
 const FORM = document.querySelector('form')
 const SEARCH_ICON = document.querySelector('.search-icon')
 const IMPUT_SEARCH = document.querySelector('.input-search')
-
+const BODY = document.querySelector('body')
 /* burger MENU */
 
 const BTN_burger = document.querySelector('.toggle')
 const BURGER_MENU = document.querySelector('.burger__div')
-
+const CATEGORY_MENU = document.querySelectorAll('burguer__li1')
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
@@ -30,13 +30,10 @@ const deployMenu = (e) => {
     if (e.target.matches('.toggle *') || e.target.matches('.toggle')) {
 
         BTN_burger.classList.toggle('toggle1')
-        BURGER_MENU.classList.toggle('burger__active')
+        BURGER_MENU.classList.toggle('burger__menu-active')
+        BODY.classList.toggle('block-scroll')
     }
-
 }
-
-
-
 
 const deploySearch = (e) => {
 
@@ -58,7 +55,8 @@ const init = () => {
 
     SEARCH_ICON.addEventListener('click', deploySearch);
     IMPUT_SEARCH.addEventListener('keyup', searching);
-    BTN_burger.addEventListener('click', deployMenu)
+    BTN_burger.addEventListener('click', deployMenu);
+
 }
 
 

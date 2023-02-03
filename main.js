635fdbc9b6596b0df92ef8c4e81957604db37f46
andres mercado <andres95rino@gmail.com>
2023-02-03 17:33:29 -0300

    Menu-ready

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 /* SEARCH */
 const FORM = document.querySelector('form')
@@ -8,7 +10,8 @@ const BODY = document.querySelector('body')
 
 const BTN_burger = document.querySelector('.toggle')
 const BURGER_MENU = document.querySelector('.burger__div')
-const CATEGORY_MENU = document.querySelectorAll('burguer__li1')
+const SUBMENU_BTN = document.querySelectorAll('.submenu_link')
+const DIV_SUBMENU = document.querySelector('.burger__ul2')
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
@@ -24,14 +27,13 @@ const swiper = new Swiper('.swiper', {
     }
 });
 
-
 const deployMenu = (e) => {
 
     if (e.target.matches('.toggle *') || e.target.matches('.toggle')) {
 
         BTN_burger.classList.toggle('toggle1')
         BURGER_MENU.classList.toggle('burger__menu-active')
-        BODY.classList.toggle('block-scroll')
+
     }
 }
 
@@ -45,18 +47,41 @@ const deploySearch = (e) => {
 
 const searching = (e) => {
 
+}
+
+function deploySubMenu(e) {
+    e.preventDefault()
+
+    const childMenu = this.nextElementSibling
+    const height1 = childMenu.scrollHeight
+    console.log(childMenu.children.length)
+    console.log(height1)
+
+    if (childMenu.classList.contains('desplegar')) {
+        childMenu.classList.remove('desplegar')
+        childMenu.removeAttribute('style')
+
+    } else {
+        childMenu.classList.add('desplegar')
+        childMenu.style.height = `${height1 * childMenu.children.length * 2}px`
+
+    }
+
+
+
+
+
+
 
 
 }
-
-
 
 const init = () => {
 
     SEARCH_ICON.addEventListener('click', deploySearch);
     IMPUT_SEARCH.addEventListener('keyup', searching);
     BTN_burger.addEventListener('click', deployMenu);
-
+    SUBMENU_BTN.forEach(e => e.addEventListener('click', deploySubMenu))
 }
 
 
